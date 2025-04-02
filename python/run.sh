5ddbcac0bde20ca7fb92564b5230683dc2a1b32e
wk989898 <nhsmwk@gmail.com>
2025-04-02 10:25:37 +0000

    update

--- python/run.sh
+++ python/run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 tiup playground nightly &
-python feishu.py
+
+sleep 5
+echo "Verifying TiDB is started..."
+TIDB_HOST=127.0.0.1
+TIDB_PORT=4000
+i=0
+while ! mysql -uroot -h${TIDB_HOST} -P${TIDB_PORT} --default-character-set utf8mb4 -e 'select * from mysql.tidb;'; do
+	i=$((i + 1))
+	if [ "$i" -gt 60 ]; then
+		echo 'Failed to start upstream TiDB'
+		exit 2
+	fi
+	sleep 2
+done
+
+ python feishu.py
